e8559960df46eea8e872ec812cb244e52746db7a
Gustavo <contato.gustavofernandes@hotmail.com>
2022-01-17 18:41:08 -0300

    adding dotenv

--- src/index.js
+++ src/index.js
@@ -7,11 +7,17 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan("tiny"));
+// app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.status(200).send("working! 🌱⛈");
+  res.status(200).json({ message: "working! 🌱⛈" });
+});
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json(`working! -  ${id} 🌱⛈`);
 });
 
 app.listen(PORT, () => {
